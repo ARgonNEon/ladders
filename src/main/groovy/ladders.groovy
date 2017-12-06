8166e29cdfcce98295b19d2cb1f5ec7538931bb0
Arne Sachtler <Arne.Sachtler@dlr.de>
2017-12-06 01:05:11 +0100

    Cleanup

--- src/main/groovy/ladders.groovy
+++ src/main/groovy/ladders.groovy
@@ -1,12 +1,3 @@
-import javax.rmi.CORBA.Util
-
-//db = new WordDB()
-//
-//println db.countWordsForLength()
-//
-//println db.isCharListValid(Utils.countChars('guacamole'))
-//
-
 
 def ladders = new Ladders()
 
