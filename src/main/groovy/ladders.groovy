209d0af53356297f9be4737cb5621512f708db05
Arne Sachtler <arne.sachtler@dlr.de>
2017-12-07 12:25:38 +0100

    Use groovy file access

--- src/main/groovy/ladders.groovy
+++ src/main/groovy/ladders.groovy
@@ -1,9 +1,6 @@
 def ladders = new Ladders()
 println "Seach tree for word lookup created. Starting search..."
-println ""
-println "Found solution:"
-
-def outfile = new File("output.txt")
+println() 
 
 new File("output.txt").withWriter {w ->
     ladders.ladders(args[0], args[1]).each {
@@ -11,5 +8,5 @@ new File("output.txt").withWriter {w ->
     w << "$it\n"
 }}
 
-println ""
+println()
 println "Solution written to output.txt"
