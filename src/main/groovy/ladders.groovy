8553447ac540d86c4e43c2047f436efd5e1558d7
Arne Sachtler <arne.sachtler@dlr.de>
2017-12-07 12:12:30 +0100

    Write output file

--- src/main/groovy/ladders.groovy
+++ src/main/groovy/ladders.groovy
@@ -1,3 +1,15 @@
 def ladders = new Ladders()
 println "Seach tree for word lookup created. Starting search..."
-ladders.ladders(args[0], args[1]).each {println it}
+println ""
+println "Found solution:"
+
+def outfile = new File("output.txt")
+
+new File("output.txt").withWriter {w ->
+    ladders.ladders(args[0], args[1]).each {
+    println it
+    w << "$it\n"
+}}
+
+println ""
+println "Solution written to output.txt"
